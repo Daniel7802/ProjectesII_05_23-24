83a508b1749ffa94f2d609d2cba6915e25d15c49
AjandroMLG <alejandro.martinez.membrilla@enti.cat>
2023-11-02 23:35:36 +0100

    boomerang
    done pa mañana

--- Assets/Project/Scripts/BoomerangThrow.cs
+++ Assets/Project/Scripts/BoomerangThrow.cs
@@ -10,18 +10,27 @@ public class BoomerangThrow : MonoBehaviour
     [SerializeField]
     GameObject source;
 
+
     private bool wantsToThrow;
+
+    [SerializeField]
+    float timer = 3.0f;
 
     [SerializeField]
     private float throwDuration = 0.5f;
     private float currentLerpValue;
+    [SerializeField]
+    private float rotationSpeed;
 
     [SerializeField]
     private float distancia;
 
+    [SerializeField]
+    private bool coming = false;
 
     public bool IsFlying = false;
 
+    [SerializeField]
     Vector2 p0, p2;
 
     // Start is called before the first frame update
@@ -33,28 +42,36 @@ public class BoomerangThrow : MonoBehaviour
     private void Update()
     {
         wantsToThrow |= Input.GetMouseButtonDown(0) && !IsFlying;
+        //TimerBoomerang();
     }
     private void FixedUpdate()
     {
-        p0 = source.transform.position;
-
+        if (!IsFlying)
+        {
+            p0 = source.transform.position;
+        }
         if (wantsToThrow && !IsFlying)
         {
             currentLerpValue = 0f;
             wantsToThrow = false;
             ThrowBoomerang();
         }
-        if (IsFlying)
+        if (IsFlying )
         {
+            transform.Rotate(0f, 0f, rotationSpeed, Space.Self);
+            if(coming)
+                p0 = source.transform.position;
+            timer = 3.0f;
             currentLerpValue = Mathf.Min(currentLerpValue + Time.fixedDeltaTime, throwDuration);
            
             float factor = currentLerpValue / throwDuration;
 
             factor = -Mathf.Abs(-1 + factor * 2) + 1;
-            if (factor > 0.6)
-                _targetJoint.maxForce = 20;
-            else
-                _targetJoint.maxForce = 4000;
+
+            if (factor >= 0.8f)
+            {
+                coming = true;
+            }
             Vector3 finalPos = Vector3.Lerp(p0, p2, factor);
 
             _targetJoint.anchor = Vector3.zero;
@@ -70,16 +87,30 @@ public class BoomerangThrow : MonoBehaviour
     void ThrowBoomerang()
     {     
 
-        p0 = transform.position; // 5,5  
+        p0 = transform.position; // 5,5     
         var auxPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         var auxVector = (Vector2)auxPoint-p0;
         auxVector.Normalize();
-        p2 = (auxVector) * distancia + (Vector2)transform.position;
+        var auxVector2 = (auxVector) * distancia + (Vector2)transform.position;
+        if (Vector2.Distance(transform.position, auxVector2) <= Vector2.Distance(transform.position, auxPoint))
+            p2 = auxVector2;
+        else
+            p2 = auxPoint;
         Debug.Log(p0 + " " + auxPoint);
         IsFlying = true;
         Debug.Log(p2);
     }
 
+    //void TimerBoomerang()
+    //{
+    //    if (staticPoint && timer >= 0.5f)
+    //    {
+    //        timer -= Time.deltaTime;
+
+    //    }
+    //    else
+    //        staticPoint = false;    
+    //}
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.yellow;
