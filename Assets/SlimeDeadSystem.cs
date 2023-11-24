5bfa51c110354b740a518972581883613d0dbaa9
AjandroMLG <alejandro.martinez.membrilla@enti.cat>
2023-11-24 16:27:56 +0100

    SlimesInstance2MiniSlimes(nowYes)

--- Assets/SlimeDeadSystem.cs
+++ Assets/SlimeDeadSystem.cs
@@ -6,7 +6,8 @@ public class SlimeDeadSystem : DeadSystem
 {
     [SerializeField] GameObject miniSlime;
     bool miniSlimesSpawned = false;
-
+    public GameObject player;
+    private Vector3 vectorBetweenPlayer, vectorSlime1, vectorSlime2;
 
     public override void Dead()
     {
@@ -18,22 +19,39 @@ public class SlimeDeadSystem : DeadSystem
         //Vector2 dir = new Vector2(0, 3);
         if(!miniSlimesSpawned)
         {
-            GameObject slime = Instantiate(miniSlime);
-            slime.transform.position = transform.position;
+            CalculateVectors();
+            Vector2 vector = transform.position;
+            GameObject slime = Instantiate(miniSlime, vector, Quaternion.identity);
+            GameObject slime2 = Instantiate(miniSlime, vector, Quaternion.identity);
+            slime.GetComponent<SlimeIA>().player = player;
+            slime2.GetComponent<SlimeIA>().player = player;
+            slime.GetComponent<Enemy>().knockbackForce = 10;
+            slime2.GetComponent<Enemy>().knockbackForce = 10;
+            slime.GetComponent<Enemy>().KnockBack(vectorSlime1);
+            slime2.GetComponent<Enemy>().KnockBack(vectorSlime2);
+
+            slime.GetComponent<Enemy>().knockbackForce = 5;
+            slime2.GetComponent<Enemy>().knockbackForce = 5;
             miniSlimesSpawned = true;
         }
        
 
        
-        //Vector2 spawnForce = dir.normalized * miniSlimeSpawnForce;
-        //rb2D.AddForce(spawnForce, ForceMode2D.Impulse);            
+       // Vector2 spawnForce = dir.normalized * miniSlimeSpawnForce;
+       // rb2D.AddForce(spawnForce, ForceMode2D.Impulse);            
 
-        //dir = new Vector2(0, -3);
-        //GameObject slime2 = Instantiate(miniSlime);
-        //slime2.transform.position = transform.position;
-        //Vector2 spawnForce2 = dir.normalized * miniSlimeSpawnForce;
-        //rb2D.AddForce(spawnForce2, ForceMode2D.Impulse);
+       // dir = new Vector2(0, -3);
+       // GameObject slime2 = Instantiate(miniSlime);
+       // Vector2 spawnForce2 = dir.normalized * miniSlimeSpawnForce;
+       //rb2D.AddForce(spawnForce2, ForceMode2D.Impulse);
 
+    }
+
+    void CalculateVectors()
+    {
+        vectorBetweenPlayer = player.transform.position - this.transform.position;
+        vectorSlime1 = new Vector2(-vectorBetweenPlayer.y, vectorBetweenPlayer.x);
+        vectorSlime2 = new Vector2(vectorBetweenPlayer.y, -vectorBetweenPlayer.x);
     }
 }
 
