d058a3e4e29bdbfc2c749593d9dc55b8d0434cc6
Marc Climent Molist <marc.climent@enti.cat>
2023-12-15 20:10:06 +0100

    54

--- Assets/Project/Scripts/BoomerangScripts/BoomerangThrow.cs
+++ Assets/Project/Scripts/BoomerangScripts/BoomerangThrow.cs
@@ -254,6 +254,7 @@ public class BoomerangThrow : MonoBehaviour
         }
         if (collision.gameObject.CompareTag("Player") && (coming ||cancelled) )
         {
+            
             cancelled = false;
             isFlying = false;
             coming = false;
