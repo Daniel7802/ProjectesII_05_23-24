3d727dc50c2e8b834391f0cf5358867a0c35e6b3
Marc Climent <marc.climent@enti.cat>
2024-01-03 11:25:50 +0100

    gitflow-feature-stash: enemies-update

--- Assets/Project/Scripts/Enemies/WaveDamageSystem.cs
+++ Assets/Project/Scripts/Enemies/WaveDamageSystem.cs
@@ -1,3 +1,4 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,19 +6,17 @@ using UnityEngine;
 public class WaveDamageSystem : DamageSystem
 {
     [SerializeField] private float knockbackForce = 30f;
-    void OnTriggerEnter2D(Collider2D collision)
+    public void DamageItem(Collider2D col)
     {
-        if (collision.gameObject.TryGetComponent<HealthSystem>(out HealthSystem hs))
+        Debug.Log(col.transform.name);
+        if (col.gameObject.TryGetComponent<HealthSystem>(out HealthSystem hs))
         {
-            
-
-            if (collision.gameObject.TryGetComponent<PlayerHealthSystem>(out PlayerHealthSystem phs) && collision.tag.Equals("Player"))
+            if (col.gameObject.TryGetComponent<PlayerHealthSystem>(out PlayerHealthSystem phs) && col.tag.Equals("Player"))
             {
-                if (phs.isInvincible == false && phs.counter < 5)
+                if (phs.isInvincible == false)
                 {
-                    phs.deleteHeart();
-                    phs.turnInvincible();
-                    Vector2 dir = new Vector2(phs.transform.position.x - transform.position.x, phs.transform.position.y-transform.position.y);
+                  
+                    Vector2 dir = new Vector2(phs.transform.position.x - transform.position.x, phs.transform.position.y - transform.position.y);
                     Vector2 kbForce = dir.normalized * knockbackForce;
                     Rigidbody2D rb2D = phs.GetComponent<Rigidbody2D>();
                     rb2D.AddForce(kbForce, ForceMode2D.Impulse);
