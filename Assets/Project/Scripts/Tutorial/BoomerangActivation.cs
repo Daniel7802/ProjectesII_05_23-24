b0bcf8d57f17596327e72b32d314cdac1c54b2f8
Dani <daniel.munoz.jimenez@enti.cat>
2024-01-07 23:37:39 +0100

    fixes

--- Assets/Project/Scripts/Tutorial/BoomerangActivation.cs
+++ Assets/Project/Scripts/Tutorial/BoomerangActivation.cs
@@ -7,13 +7,13 @@ public class BoomerangActivation : MonoBehaviour
     [SerializeField]
     GameObject boomerang;
 
-    [SerializeField]
-    GameObject boomerangHUD;
+    //[SerializeField]
+    //GameObject boomerangHUD;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         boomerang.SetActive(true);
-        boomerangHUD.SetActive(true);
+        //boomerangHUD.SetActive(true);
         Destroy(gameObject);
     }
 }
