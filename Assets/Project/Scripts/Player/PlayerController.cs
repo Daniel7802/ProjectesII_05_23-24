be4a445242d09b7585f50ee4d5e7ad3d2820cbd2
AjandroMLG <alejandro.martinez.membrilla@enti.cat>
2024-02-23 13:12:04 +0100

    Ice

--- Assets/Project/Scripts/Player/PlayerController.cs
+++ Assets/Project/Scripts/Player/PlayerController.cs
@@ -5,11 +5,33 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // SCRIPT SOLO PARA CONTROLAR LOS ESTADOS DEL PLAYER
+    Rigidbody2D _rb;
 
     public enum PlayerStates { NONE, CINEMATIC }
     public PlayerStates playerStates;
 
     // Variables de scripts del player
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "IceGround")
+        {
+            _rb.drag = 5;
+        }
+            
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "IceGround")
+        {
+            _rb.drag = 20;
+        }
+
+    }
     // Variables para poder acceder desde otros scripts
 }
