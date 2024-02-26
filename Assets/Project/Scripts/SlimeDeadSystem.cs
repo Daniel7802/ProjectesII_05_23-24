0b789016f45e7c95f66306db18216c09716f6507
Marc Climent Molist <marc.climent@enti.cat>
2024-02-26 17:38:14 +0100

    ENEMY FIX

--- Assets/Project/Scripts/SlimeDeadSystem.cs
+++ Assets/Project/Scripts/SlimeDeadSystem.cs
@@ -25,27 +25,23 @@ public class SlimeDeadSystem : DeadSystem
 
         if (!miniSlimesSpawned)
         {
-
-
             GameObject minislime1 = Instantiate(miniSlime, transform.position, Quaternion.identity);
             GameObject minislime2 = Instantiate(miniSlime, transform.position, Quaternion.identity);
-            CalculateVectors();
-            minislime1.gameObject.GetComponent<Rigidbody2D>().AddForce(vectorSlime1, ForceMode2D.Impulse);
-            minislime2.gameObject.GetComponent<Rigidbody2D>().AddForce(vectorSlime2, ForceMode2D.Impulse);
+            
             minislime1.gameObject.GetComponent<HealthSystem>().TurnInvencible(true);
             minislime2.gameObject.GetComponent<HealthSystem>().TurnInvencible(true);
-            
+
+            player = GetComponent<DetectionZone>().player;
+            vectorBetweenPlayer = player.transform.position - transform.position;
+            vectorSlime1 = new Vector2(-vectorBetweenPlayer.y, vectorBetweenPlayer.x).normalized * spawnForce;
+            vectorSlime2 = new Vector2(vectorBetweenPlayer.y, -vectorBetweenPlayer.x).normalized * spawnForce;
+            minislime1.gameObject.GetComponent<Rigidbody2D>().AddForce(vectorSlime1, ForceMode2D.Impulse);
+            minislime2.gameObject.GetComponent<Rigidbody2D>().AddForce(vectorSlime2, ForceMode2D.Impulse);
+
             miniSlimesSpawned = true;
         }
     }
 
 
-    void CalculateVectors()
-    {
-        player = GetComponent<DetectionZone>().player;
-        vectorBetweenPlayer = player.transform.position - transform.position;
-        vectorSlime1 = new Vector2(-vectorBetweenPlayer.y, vectorBetweenPlayer.x).normalized * spawnForce;
-        vectorSlime2 = new Vector2(vectorBetweenPlayer.y, -vectorBetweenPlayer.x).normalized * spawnForce;
-    }
 }
 
