e9486076cf3b2ccadaf659a977d3064b9c05d477
Dani ENTI <daniel.munoz.jimenez@enti.cat>
2024-03-01 19:58:42 +0100

    Player Inventory

--- Assets/Project/Scripts/Player/PlayerInventory.cs
+++ Assets/Project/Scripts/Player/PlayerInventory.cs
@@ -1,37 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerInventory : MonoBehaviour
 {
+    public int currentRoots = 30;
+
+    [SerializeField]
+    GameObject[] extraHearts;
+
+    private int extraheartsCounter = 0;
+
+    public TextMeshProUGUI[] currentRootsText;
+
+    private PlayerHealthSystem phs;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < currentRootsText.Length; i++)
+        {
+            currentRootsText[i].text = currentRoots.ToString();
+        }
+
+        phs = this.GetComponent<PlayerHealthSystem>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        for (int i = 0; i < currentRootsText.Length; i++)
+        {
+            currentRootsText[i].text = currentRoots.ToString();
+        }
     }
 
-    private void IncreaseMaxHearts()
+    public void IncreaseMaxHearts()
     {
-        //if (currentRoots >= 5)
-        //{
-        //    currentRoots -= 5;
+        if (currentRoots >= 5)
+        {
+            currentRoots -= 5;
 
-        //    for (int i = 0; i < currentRootsText.Length; i++)
-        //    {
-        //        currentRootsText[i].text = currentRoots.ToString();
-        //    }
+            for (int i = 0; i < currentRootsText.Length; i++)
+            {
+                currentRootsText[i].text = currentRoots.ToString();
+            }
 
-        //    phs.MaxHealth++;
-        //    extraHearts[extraheartsCounter].SetActive(true);
-        //    extraheartsCounter++;
-        //    phs.health = phs.MaxHealth;
-        //    phs.RespawnHeal();
-        //}
+            phs.MaxHealth++;
+            extraHearts[extraheartsCounter].SetActive(true);
+            extraheartsCounter++;
+            phs.health = phs.MaxHealth;
+            phs.RespawnHeal();
+        }
     }
 }
