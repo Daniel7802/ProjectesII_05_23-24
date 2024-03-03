7bce7da397d03b27c4fdc97cb8395fcfbf9d9e02
AjandroMLG <alejandro.martinez.membrilla@enti.cat>
2024-03-03 23:47:37 +0100

    psheito

--- Assets/Project/Scripts/SceneManagement/SceneChanger.cs
+++ Assets/Project/Scripts/SceneManagement/SceneChanger.cs
@@ -7,9 +7,21 @@ public class SceneChanger : MonoBehaviour
 {
     [SerializeField]
     private string scene;
+    [SerializeField]
+    private GameObject hud;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        SceneManager.LoadScene(scene);
+        FadeInOutManager.instance.Fadein();
+        hud.SetActive(false);   
+        StartCoroutine(StartGameCorrotine(scene));
+    }
+
+
+    private IEnumerator StartGameCorrotine(string a)
+    {
+        Debug.Log("Waiting for loadScene");
+        yield return new WaitWhile(delegate { return FadeInOutManager.instance.fadeIn; });
+        SceneManager.LoadScene(a);
     }
 }
